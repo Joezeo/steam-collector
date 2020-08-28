eefe2f71e099e0300df92acfacc2848b9dcaf3cc
zhaozhe <1252795700@qq.com>
2020-08-28 14:34:36 +0800

    init repository

---
+++ src/main/java/com/joezeo/steamlookup/SteamlookupApplication.java
@@ -0,0 +1,13 @@
+package com.joezeo.steamlookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamlookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamlookupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joezeo/steamlookup/SteamlookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joezeo.steamlookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamlookupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
