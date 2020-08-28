478a1cd4ce534d75b05695de163bfe7ce6b6a28b
zhaozhe <1252795700@qq.com>
2020-08-28 15:41:08 +0800

    modify project name.

--- src/main/java/com/joezeo/steamcollector/SteamCollectorApplication.java
+++ src/main/java/com/joezeo/steamcollector/SteamCollectorApplication.java
@@ -1,13 +1,13 @@
-package com.joezeo.steamlookup;
+package com.joezeo.steamcollector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteamlookupApplication {
+public class SteamCollectorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SteamlookupApplication.class, args);
+        SpringApplication.run(SteamCollectorApplication.class, args);
     }
 
 }
