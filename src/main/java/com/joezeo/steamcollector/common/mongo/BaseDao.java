88a2af4a0c0fe506d21045333b1bc9cf0d75a08e
zhaozhe <1252795700@qq.com>
2020-09-04 10:03:27 +0800

    add mongo base dao class.

--- src/main/java/com/joezeo/steamcollector/common/mongo/BaseDao.java
+++ src/main/java/com/joezeo/steamcollector/common/mongo/BaseDao.java
@@ -1,5 +1,9 @@
 package com.joezeo.steamcollector.common.mongo;
 
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Dao类对象的基类
  * 所有dao都应继承此类
@@ -8,6 +12,19 @@ package com.joezeo.steamcollector.common.mongo;
  * @email joezane.cn@gmail.com
  * @date 2020/9/2 18:32
  */
-public class BaseDao<T extends IDBDocument> {
+public abstract class BaseDao<T extends IDBDocument> {
 
+    @Autowired
+    protected Datastore datastore;
+
+    protected Class<T> clazz;
+
+    public BaseDao<T> of (Class<T> clazz) {
+        this.clazz = clazz;
+        return this;
+    }
+
+    public Query<T> query() {
+        return datastore.createQuery(clazz);
+    }
 }
--- src/main/java/com/joezeo/steamcollector/common/mongo/IDBDocument.java
+++ src/main/java/com/joezeo/steamcollector/common/mongo/IDBDocument.java
@@ -1,5 +1,7 @@
 package com.joezeo.steamcollector.common.mongo;
 
+import java.io.Serializable;
+
 /**
  * 所有mongo集合对应的vo对象都应该继承此类
  *
@@ -7,6 +9,6 @@ package com.joezeo.steamcollector.common.mongo;
  * @email joezane.cn@gmail.com
  * @date 2020/9/2 18:32
  */
-public interface IDBDocument {
+public interface IDBDocument extends Serializable {
 
 }
