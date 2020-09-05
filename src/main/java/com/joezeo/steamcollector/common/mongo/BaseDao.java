faf4d22763db886ba75e24f38577d09201dfbb24
zhaozhe <1252795700@qq.com>
2020-09-05 17:18:17 +0800

    modify BaseDao,IDBDocument

--- src/main/java/com/joezeo/steamcollector/common/mongo/BaseDao.java
+++ src/main/java/com/joezeo/steamcollector/common/mongo/BaseDao.java
@@ -2,17 +2,21 @@ package com.joezeo.steamcollector.common.mongo;
 
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Dao类对象的基类
  * 所有dao都应继承此类
  *
+ * 泛型：
+ *      K：表示集合_id属性的类型
+ *      T：表示数据库集合对应的vo对象类
  * @author ZhaoZhe
  * @email joezane.cn@gmail.com
  * @date 2020/9/2 18:32
  */
-public abstract class BaseDao<T extends IDBDocument> {
+public abstract class BaseDao<K,T extends IDBDocument<K>> {
     {
         of();
     }
@@ -29,19 +33,32 @@ public abstract class BaseDao<T extends IDBDocument> {
         this.clazz = clazz;
     }
 
-    public Query<T> query() {
+    /** add */
+    public abstract boolean add(IDBDocument<K> document);
+
+    /** delete */
+    public abstract boolean delete(IDBDocument<K> document);
+
+    /** update */
+
+    protected abstract boolean update(IDBDocument<K> document);
+
+    protected UpdateOperations<T> ops() {
+        return datastore.createUpdateOperations(clazz);
+    }
+
+    protected boolean up(UpdateOperations<T> ops) {
+        return false;
+    }
+
+    protected boolean safeUp(UpdateOperations<T> ops) {
+        return false;
+    }
+
+    /** query */
+    public abstract IDBDocument<K> query(K id);
+
+    protected Query<T> query() {
         return datastore.createQuery(clazz);
-    }
-
-    public boolean delete(IDBDocument document) {
-        return false;
-    }
-
-    public boolean up(IDBDocument document) {
-        return false;
-    }
-
-    public boolean safeUp(IDBDocument document) {
-        return false;
     }
 }
